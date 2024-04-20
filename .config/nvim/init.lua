514dbc1168709edcd115be349cd1ee9d871de044
Satanx016 <z.dadsi21@gmail.com>
2024-04-20 14:05:05 +0100

    Refactor

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,32 +1,32 @@
-vim.g.base46_cache = vim.fn.stdpath "data" .. "/otsu/base46/"
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/otsu/base46/"
 vim.g.mapleader = " "
 
 -- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-local lazy_config = require "configs.lazy"
+local lazy_config = require("configs.lazy")
 
-require "options"
+require("options")
 
 -- load plugins
 require("lazy").setup({
-  { import = "otsu.plugins" },
-  { import = "plugins" },
+	{ import = "otsu.plugins" },
+	{ import = "plugins" },
 }, lazy_config)
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
 dofile(vim.g.base46_cache .. "statusline")
 
-require "otsu.autocmds"
+require("otsu.autocmds")
 
 vim.schedule(function()
-  require "mappings"
+	require("mappings")
 end)
