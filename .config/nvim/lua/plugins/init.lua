9589c58e14d48e53a91aa7c40e1f403b111df91a
Satanx016 <z.dadsi21@gmail.com>
2024-04-24 11:46:02 +0100

    Refactor

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,16 +1,13 @@
+-- Plugins are added and tested here, might be merged in otsu
 return {
-	{
-		"stevearc/conform.nvim",
-		-- event = 'BufWritePre', -- uncomment for format on save
-		config = function()
-			require("configs.conform")
-		end,
-	},
 	{
 		"folke/persistence.nvim",
 		event = "BufReadPre", -- this will only start session saving when an actual file was opened
-		config = function()
+        opts = function ()
 			require("configs.persistence")
+        end,
+		config = function(_, opts)
+			require("persistence").setup(opts)
 		end,
 	},
 
