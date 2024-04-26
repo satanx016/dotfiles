12a7ee6de78274c001dbc58e636292a10e70b2f0
Satanx016 <z.dadsi21@gmail.com>
2024-04-26 04:17:47 +0100

    Add external repository as a submodule

--- .config/nvim/lua/configs/persistence.lua
+++
@@ -1,8 +0,0 @@
-local options = {
-	dir = vim.fn.expand(vim.fn.stdpath("state") .. "/sessions/"), -- directory where session files are saved
-	options = { "buffers", "curdir", "tabpages", "winsize" }, -- sessionoptions used for saving
-	pre_save = nil, -- a function to call before saving the session
-	save_empty = false, -- don't save if there are no open file buffers
-}
-
-require("persistence").setup(options)
