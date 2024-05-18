9030fb4a83ce3485f49c8977059fa835bc8baa34
Satanx016 <z.dadsi21@gmail.com>
2024-05-18 12:22:37 +0100

    fish: better modular way to call waifu's popups

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,31 +6,15 @@ alias yeet 'yay -Rnsc'
 alias crap 'yay -Qdtq'
 
 alias nv nvim
-alias nvroot 'sudo -E -s nvim'
 
 alias neo 'command clear && fastfetch'
 alias tb taskbook
 
-function otsu_clear_pop
-    command clear
-    # chafa ~/Pictures/Characters/ZeroTsu/hmm.png --size=12
-    chafa ~/Pictures/Characters/Ganyu/ganyuEating --size=14
-    echo
-    # echo "↳ Sometimes... I wonder... If i'll ever could be a human like you ?"
-    echo "↳ I don't understand why people insist on going all out to try and prove how their existence is more valuable than everyone else's."
-end
-
-function otsu_pop
-    # chafa ~/Pictures/Characters/ZeroTsu/hmm.png --size=12
-    chafa ~/Pictures/Characters/Ganyu/ganyuEating --size=14
-    echo
-    # echo '↳ hmm...'
-    echo '↳ I can fix this...'
-end
+source ~/.config/fish/./conf.d/waifus/ganyu.fish
 
 if status is-interactive; and not status is-login
-    alias clear otsu_clear_pop
-    otsu_pop
+    alias clear waifu_clear_pop
+    waifu_pop
     starship init fish | source
     zoxide init fish | source
 end
