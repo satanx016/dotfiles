d0715615184e0977346161469605f2e8f2b92880
Satanx016 <z.dadsi21@gmail.com>
2024-08-31 15:55:05 +0100

    fish: update refs and add new zero_tsu pop

--- .config/fish/conf.d/waifus/ganyu.fish
+++ .config/fish/conf.d/waifus/ganyu.fish
@@ -1,12 +1,12 @@
 function waifu_clear_pop
     command clear
-    chafa ~/Pictures/Characters/Ganyu/ganyuEating --size=14
+    chafa ~/Pictures/Characters/Ganyu/ganyuEating.png --size=14
     echo
     echo "↳ I don't understand why people insist on going all out to try and prove how their existence is more valuable than everyone else's."
 end
 
 function waifu_pop
-    chafa ~/Pictures/Characters/Ganyu/ganyuEating --size=14
+    chafa ~/Pictures/Characters/Ganyu/ganyuEating.png --size=14
     echo
     echo '↳ I can fix this...'
 end
