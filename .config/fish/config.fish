a931115cb46a439b738282e15e6a47c91246cb0c
Satanx016 <z.dadsi21@gmail.com>
2024-10-23 05:05:32 +0100

    refactor(env-vars): move IME(fcitx5) vars to hyprland conf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,5 @@
 set -x EDITOR nvim
 set -x VISUAL nvim
-# IME for 日本語 ⛩️
-set -x GTK_IM_MODULE fcitx
-set -x QT_IM_MODULE fcitx
-set -x XMODIFIERS "@im=fcitx"
 
 source ~/.config/fish/./conf.d/waifus/ganyu.fish
 
