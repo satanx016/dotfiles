869d64e10f7610b921bd501d22811e23e57f1832
Satanx016 <z.dadsi21@gmail.com>
2024-12-08 18:53:53 +0100

    feat(fish): Add eza a modern alternative to ls && organize aliases

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,10 +1,13 @@
 # essentials
-
+alias eza 'eza --icons=auto --classify --group-directories-first'
+alias ls 'eza'
+alias tree 'eza -T'
 alias rm 'rm -r'
+
 alias yeet 'yay -Rnsc'
 alias crap 'yay -Qdtq'
-alias badapple 'notify-send \'This is what I call cool stuff 😈 \' && CACA_DRIVER=ncurses mpv -vo=caca https://youtu.be/FtutLA63Cp8'
 
-alias nv nvim
-
+alias nv 'nvim'
 alias neo 'command clear && fastfetch'
+
+alias badapple 'notify-send \'This is what I call cool stuff 😈 \' && CACA_DRIVER=ncurses mpv -vo=caca https://youtu.be/FtutLA63Cp8'
