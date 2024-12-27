714c5ab7f5d4a187cdbfde57188f7a4d6c3210d2
Satanx016 <z.dadsi21@gmail.com>
2024-12-27 03:50:38 +0100

    feat(fish): pywal support

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ set PATH $PATH /home/satanx016/.local/bin
 set -x EDITOR nvim
 set -x VISUAL nvim
 
-source ~/.config/fish/./conf.d/waifus/ganyu.fish
+cat ~/.cache/wal/sequences &
 
 # Only init waifu popups and zoxide if the shell is interactive and not a login one
 if status is-interactive; and not status is-login
