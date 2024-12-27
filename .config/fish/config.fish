6ba99066ae237b782eb025b69dc8410659feae70
Satanx016 <z.dadsi21@gmail.com>
2024-12-27 03:49:51 +0100

    perf(fish): do the waifu sourcing conditionally

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ source ~/.config/fish/./conf.d/waifus/ganyu.fish
 
 # Only init waifu popups and zoxide if the shell is interactive and not a login one
 if status is-interactive; and not status is-login
+    source ~/.config/fish/./conf.d/waifus/ganyu.fish
     alias clear waifu_clear_pop
     waifu_pop
     starship init fish | source
